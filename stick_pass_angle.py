3947130d8e01204be18f1b1d6037080ac5556db7
stwuiwmoc <stwuiwmoc5929@gmail.com>
2021-04-28 18:05:10 +0900

    1. 角度をdeg->rad 2. stick_plotのinput変更 3. stick_plotにlegend追加 4. fem->actの変換係数を追加 5. text_plotで傾きの差分も表示 ||| 1. 角度小さすぎてdegでもradでも直感的にわからんので伝わりやすい方で 2. stick_plot内部でtangent_lineを呼び出して内部で接線を作成してplotするように変更 3. 角度はplotのlegendだけに表示して、textにはpxのみ表示（かえるかも） 4. 鍵谷先生メールにあった変換係数を導入（fem:0.05Nm加えた時のamplitude -> act 100step(1mm)動かした時のamplitude） 5. text_plotにcenterとlimbの傾きの差分も追記（pxで表示）

--- stick_pass_angle.py
+++ stick_pass_angle.py
@@ -8,7 +8,6 @@ Created on Thu Apr 22 14:48:33 2021
 import numpy as np
 import pandas as pd
 import os
-import pykrige as krg
 import proper as pr
 import PIL
 import fits_correlation as corr
@@ -79,38 +78,39 @@ def tangent_line(x, y_surf, edge):
     
     idx = abs( x - (radi-edge)).argmin()
     
-    tilt = ( y[idx+10] - y[idx-10] ) / ( x[idx+10] - x[idx-10] )
-    tilt_deg = np.rad2deg(np.arctan(tilt)) # 角度deg
+    tilt = ( y[idx+15] - y[idx-15] ) / ( x[idx+15] - x[idx-15] )
+    tilt_rad = np.arctan(tilt) # 角度rad
     y_line = tilt * (x - x[idx]) + y[idx]
-    return y_line, tilt_deg, idx
+    return y_line, tilt_rad, idx
 
-def calc_zwopx(tilt_deg):
+def calc_zwopx(tilt_rad):
     f = 500e3 # 望遠鏡の焦点距離 [um]
     zwopx = 2.4 # zwo183 : 2.4um per 1px
     
-    theta = 2 * np.deg2rad(tilt_deg) # 反射するので鏡の傾きの2倍
+    theta = 2 * tilt_rad # 反射するので鏡の傾きの2倍
     physical_length = f * np.tan(theta) # 検出器位置での物理的距離
     px_num = physical_length / zwopx
     return px_num
 
-def stick_plot(fig, title, position, x, y_surf, y_center, y_edge, edge):
+def stick_plot(fig, title, position, x, y_surf, edge):
     px = len(x)
     radi = x.max()
     
-    ## center
-    idx_c = round(px/2)
-    
-    ## edge
-    idx_e = abs( x - (radi-edge)).argmin()
-    
+    y_ct, tilt_ct, idx_ct = tangent_line(x_arr, y_surf, m1_radi)
+    y_eg, tilt_eg, idx_eg = tangent_line(x_arr, y_surf, edge)
+    tilt_ct_mrad = str(round(tilt_ct * 1e6, 3))
+    tilt_eg_mrad = str(round(tilt_eg * 1e6, 3))
+
     ## plot
     ax = fig.add_subplot(position)
-    ax.plot(x[1:-1], y_surf[1:-1], linewidth=5)
-    ax.plot(x, y_center)
-    ax.plot(x, y_edge)
-    ax.scatter([x[idx_c], x[idx_e]], [y_surf[idx_c], y_surf[idx_e]], s=200, c="red")
+    ax.plot(x[1:-1], y_surf[1:-1], linewidth=5, label="")
+    ax.plot(x, y_ct, label=tilt_ct_mrad + " [micro rad]")
+    ax.plot(x, y_eg, label=tilt_eg_mrad + " [micro rad]")
+    ax.scatter([x[idx_c], x[idx_e]], [y_surf[idx_c], y_surf[idx_e]], s=200, c="red", label="")
     
     ax.set_ylim(y_surf.min(), y_surf.max())
+    ax.set_ylabel
+    ax.legend()
     return ax
 
 def table_plot(fig, title, position, data_list, col, row):
@@ -145,7 +145,15 @@ if __name__ == '__main__':
     stick_angle = 22.5 # アルミ棒のx軸に対する角度deg
     edge_length = 40 # フチから斜め鏡までの距離
     # fem では0.05Nm のトルク、実物の+-500は板バネ+-5mm相当、ばね定数5.78N/mm、腕の長さ0.25m
-    fem2act = (5*2) *  5.78 * 0.25 / 0.05
+    
+    #WH番号　[0,1,6,7,12,13,18,19,24,25,30,31]+1 にはx37
+    #WH番号 [2,3,8,9,14,15,20,21,26,27,32,33]+1 にはx20
+    #WH番号 [4,10,16,22,28,34]+1 にはx-38
+    #WH番号　[5,11,17,23,29,35]+1 にはx38
+    act_tuning = np.array([ 37.,  37.,  20.,  20., -38.,  38.,  37.,  37.,  20.,  20., -38.,
+        38.,  37.,  37.,  20.,  20., -38.,  38.,  37.,  37.,  20.,  20.,
+       -38.,  38.,  37.,  37.,  20.,  20., -38.,  38.,  37.,  37.,  20.,
+        20., -38.,  38.])
     
     fname_res = "mkfolder/fits_correlation/210423/ex10.csv"
     df_res = pd.read_csv(fname_res, index_col=0)
@@ -153,20 +161,18 @@ if __name__ == '__main__':
     x_arr = y_arr = np.linspace(-m1_radi, m1_radi, px)
     xx, yy = np.meshgrid(x_arr, y_arr)
     
-    df0 = read("_Fxx/PM3.5_36ptAxWT03_F00.smesh.txt") 
+    df0 = read("_Fxx/PM3.5_36ptAxWT06_F00.smesh.txt") 
     tf = np.where(xx**2+yy**2<=m1_radi**2, True, False)
     
     for i in range(0, len(df_res)):
-    #for i in range(0, 2):
+    #for i in range(0, 1):
         act_num = "F" + str(df_res["act"][i]).zfill(2)
         print(act_num)
+        
+        #fem は0.5Nm, モーター100step(1mm)への変換係数がact_tuning, 0423の計測は+-500stepなので更に10倍
+        fem2act = 10 * act_tuning[df_res["act"][i]-1]
     
-        ## 現実でのF09 -> モデルでのF08
-        ## 現実でのF11 -> モデルでのF07
-        #act_dict = {"F09":"F08", "F11":"F07"} # {act_num:fem_num}
-        #act_num = "F09"
-        #fname = "_Fxx/PM3.5_36ptAxWT03_" + act_dict[act_num] + ".smesh.txt"
-        fname = "_Fxx/PM3.5_36ptAxWT03_" + act_num + ".smesh.txt"
+        fname = "_Fxx/PM3.5_36ptAxWT06_" + act_num + ".smesh.txt"
         dfxx = read(fname)
         diff =  tf * fem_interpolate(df0, dfxx)
         
@@ -177,21 +183,23 @@ if __name__ == '__main__':
         y_e, tilt_e, idx_e = tangent_line(x_arr, stick_line, edge_length)
         
         ## calc tilt_angle to zwo183 px ------------------------------------------
-        zwopx_c = calc_zwopx(tilt_c)
+        zwopx_c = calc_zwopx(tilt_c) 
         zwopx_e = calc_zwopx(tilt_e)
         
         
         ## for plot ------------------------------------------------------------
-        text = ["tilt_center = " + str(tilt_c.round(6)) + " [deg]",
-                "zwopx_center = " + str(zwopx_c.round(5)) + "[px]",
-                fname_res[-15:-4] + " = " + str(round(df_res["dvert_c"][i], 5)) + "[px]",
+        text = ["zwopx_center = " + str(zwopx_c.round(3)) + "[px]",
+                fname_res[-15:-4] + " = " + str(round(df_res["dvert_c"][i], 3)) + "[px]",
                 "",
-                "tilt_edge = " + str(tilt_e.round(6)) + " [deg]",
-                "zwopx_edge = " + str(zwopx_e.round(5)) + " [px]",
-                fname_res[-15:-4] + " = " + str(round(df_res["dvert_l"][i], 5)) + "[px]",
+                "zwopx_edge = " + str(zwopx_e.round(3)) + " [px]",
+                fname_res[-15:-4] + " = " + str(round(df_res["dvert_l"][i], 3)) + "[px]",
+                "",
+                "zwopx_diff = " + str((zwopx_e-zwopx_c).round(3)) + " [px]",
+                fname_res[-15:-4] + " = " + str(round(df_res["dvert_l"][i]-df_res["dvert_c"][i], 3)) + "[px]",
                 ]
+        
         #title_diff = act_dict[act_num] + " in FEM model"
-        title_diff = act_num
+        title_diff = "act" + act_num[1:] + " in FEM"
         title_rotate = "act" + act_num[1:] + " ( FEM x " + str(fem2act) + " ), " + str(stick_angle)+" deg"
         
         fig = plt.figure(figsize=(10,10))
@@ -202,7 +210,7 @@ if __name__ == '__main__':
         ax_rotate.hlines(round(px/2), 0, px-1, linewidth=5, colors = "white")
         #ax_table = table_plot(fig, "", gs[1,0], table_list, column_list, row_list)
         ax_text = text_plot(fig, "", gs[1,0], text)
-        ax_stick = stick_plot(fig, "", gs[1, 1], x_arr, stick_line, y_c, y_e, edge_length)
+        ax_stick = stick_plot(fig, "", gs[1, 1], x_arr, stick_line, edge_length)
         fig.tight_layout()
         
         picname = mkfolder() + "act_" + act_num + ".png"
